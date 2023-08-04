9ff52162d446cd1a4c41ab86302d47125e941d1a
Rafael Travassos de Vasconcellos <travassos.rafael@gmail.com>
2023-08-04 20:53:53 -0300

    Aula 9

---
+++ ExerciciosCSharp/Cap5/OlaMundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCSharp.Cap5.OlaMundo
+{
+    class ProgramaPrincipal
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos uma variavel do tipo string (texto)
+            String Digitado = new String("");
+            //Solicitamos que o usuario digite alguma coisa e
+            //armazene na variavel criada
+            Digitado = Console.ReadLine();
+            //Mostramos o que foi digitado             
+            Console.WriteLine(Digitado);
+        }
+    }
+}
