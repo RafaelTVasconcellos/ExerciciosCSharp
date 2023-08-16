43e605c7b01b91fe4f8e3af5737965cebf68c251
Rafael Travassos de Vasconcellos <travassos.rafael@gmail.com>
2023-08-16 20:55:32 -0300

    Aula 14

---
+++ ExerciciosCSharp/Cap7/Dicionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Cap7
+{
+    class Dicionario
+    {
+        static void Main(string[] ags)
+        {
+            WriteLine("--- Uso de Dicionários ---");
+            var sexo = new Dictionary<string, string>();
+            sexo.Add("M", "Masculino");
+            sexo.Add("F", "Feminino");
+        }
+    }
+}
---
+++ ExerciciosCSharp/Cap7/ParametrosOpcionais.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Cap7
+{
+    class ParametrosOpcionais
+    {
+        // regra: obrigatórios DEVEM vir antes dos opcionais
+        static decimal Faturamento(decimal qtde, decimal preco = 10, decimal bonus = 5)
+                => qtde * preco + bonus;
+        static void Main(string[] args)
+        {
+            WriteLine("Parâmetros Opcionais");
+            WriteLine(Faturamento(500, 5, 10));
+            WriteLine("Parâmetros 1 e 2");
+            WriteLine(Faturamento(50, 12));
+            WriteLine("Parâmetros 1 e 3");
+            WriteLine(Faturamento(50, bonus: 10));
+            ReadLine();
+        }
+    }
+}
